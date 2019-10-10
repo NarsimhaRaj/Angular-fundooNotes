d8f63a159bbcbaef919cc1d449cc9d527065f8ce
MobiComp <narsimharaj.kasu01@gmail.com>
2019-10-10 17:04:44 +0530

    adding sidenav content

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, Input } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userServices/user.service';
 
 @Component({
   selector: 'app-components/dashboard',
@@ -10,30 +12,43 @@ import { map } from 'rxjs/operators';
 })
 export class DashboardComponent {
 
-  search_button:Boolean=false;
-  clickOutside:Boolean=false;
-  panelOpenState:Boolean = false;
+  search_button: Boolean = false;
+  clickOutside: Boolean = false;
+  panelOpenState: Boolean = false;
+  openNotes: Boolean = false;
 
-  pin:Boolean=false;
-  unpin:Boolean=false;
-  checkbox:Boolean=false;
-  
+  pin: Boolean = false;
+  unpin: Boolean = false;
+  checkbox: Boolean = false;
+
+  title = new FormControl('', [
+    Validators.required
+  ]);
+  description = new FormControl('', [
+    Validators.required
+  ]);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  
-  
-  constructor(private breakpointObserver: BreakpointObserver) {
-    // console.log(this.pin);
+  constructor(private breakpointObserver: BreakpointObserver, private userServices: UserService) {
+
   }
-  pinned(){
-    this.pin=!this.pin;
-  }
+  // pinned(){
+  //   this.pin=!this.pin;
+  // }
 
   onClickedOutside(e: Event) {
-    this.panelOpenState=!this.panelOpenState;
+    this.panelOpenState = !this.panelOpenState;
+  }
+  save() {
+    if (this.title.valid && this.description.valid) {
+      var notes = { title: this.title.value, description: this.description.value }
+      this.userServices.addNotes(notes);
+      this.title.setValue("");
+      this.description.setValue("");
+    }
   }
 }
