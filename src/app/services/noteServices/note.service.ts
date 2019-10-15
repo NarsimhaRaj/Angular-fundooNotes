2e10cd1113739df9b04884cb601ae3b568767311
MobiComp <narsimharaj.kasu01@gmail.com>
2019-10-15 12:02:49 +0530

    added new notes services

--- src/app/services/noteServices/note.service.ts
+++ src/app/services/noteServices/note.service.ts
@@ -17,7 +17,7 @@ export class NoteService {
 
   /**
    * @description : executes httpServices post request with token to method to add a notes 
-   * @param notes : its an object containing like title, description 
+   * @param notes : its an object containing details like title, description 
    */
   addNotes(notes) {
     let url = "/notes/addNotes";
@@ -34,22 +34,45 @@ export class NoteService {
 
   /**
    * @description : executes httpServices post request with token method to delete a notes 
-   * @param notes : its an object containing like title, description 
+   * @param notes : its an object containing details like noteIdList , isDeleted, etc. 
    */
-  deleteNotes(note) {
-
-    let data = { noteIdList: [note.id], isDeleted: true };
+  deleteNotes(data) {    
     let url = "/notes/trashNotes";
     return this.httpService.postWithToken(url, data, this.userServices.loginId);
   }
-  addToArchive(note) {
-    let data = { noteIdList: [note.id], isArchived: true };
+  /**
+   * @description to make a note archive here sending an post request to rest api with header token
+   * @param note its an object containing details like isArchived, noteIdList.
+   */
+  addToArchive(data) {
     let url = "/notes/archiveNotes";
     return this.httpService.postWithToken(url, data, this.userServices.loginId);
   }
-  updateBackgroundColor(color, note) {
-    let data = { noteIdList: [note.id], color: color };
+
+  /**
+   * @description updates background color of notes
+   * @param note its an object containing details like noteIdList, color. 
+   */
+  updateBackgroundColor(data) {
     let url = "/notes/changesColorNotes";
+    return this.httpService.postWithToken(url, data, this.userServices.loginId)
+  }
+
+  /**
+   * @description to delete a note permanently 
+   * @param data contains noteId and isDeleted key value pairs
+   */
+  deleteForeverNotes(data){
+    let url = "/notes/deleteForeverNotes";
+    return this.httpService.postWithToken(url, data, this.userServices.loginId);
+  }
+
+  /**
+   * @description to update a notes
+   * @param data contains noteId key value pairs
+   */
+  updateNotes(data){
+    let url = "/notes/updateNotes";
     return this.httpService.postWithToken(url, data, this.userServices.loginId)
   }
 }
