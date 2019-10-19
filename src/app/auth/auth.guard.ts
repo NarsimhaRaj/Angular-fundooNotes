b03ec7cd6a03196298262e70ec257bf4730f66ef
MobiComp <narsimharaj.kasu01@gmail.com>
2019-10-19 18:07:42 +0530

    cheking for user id if not null

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanDeactivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  private loginId:boolean;
-
   constructor(private route:Router) {
    }
- 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
+ /**
+  * @description canAtivate is a AuthGuard function which allows route if condition gets true
+  * @param next Contains the information about a route associated with a component loaded in an outlet at a 
+  * particular moment in time.
+  * @param state Represents the state of the router at a moment in time.
+  */
+  canActivate(): boolean {
       let user=JSON.parse(sessionStorage.getItem('user'));
       if(user!=null && user.id)
       {
