2767cef5f819ddb50905f17d77372e5351cf55db
MobiComp <narsimharaj.kasu01@gmail.com>
2019-11-02 12:14:35 +0530

    yesterday format added to yesterday dates

--- src/app/pipe/date/my-date.pipe.ts
+++ src/app/pipe/date/my-date.pipe.ts
@@ -12,11 +12,11 @@ export class MyDatePipe implements PipeTransform {
     value=value.replace("GMT+0000", "GMT+0530");
 
     var todayFormat=new DatePipe('en-US').transform(today, "MMM d, y");
-    var remiderDate=new DatePipe('en-US').transform(value, "MMM d, y");
+    var reminderDate=new DatePipe('en-US').transform(value, "MMM d, y");
     
     var time=new Date(value).toLocaleString("en-US", {hour: "numeric", minute: "numeric", hour12: true});
 
-    if(todayFormat==remiderDate)
+    if(todayFormat==reminderDate)
     {
       return "today"+" "+time;
     }
@@ -24,11 +24,18 @@ export class MyDatePipe implements PipeTransform {
     var tomorrow=today.setDate(today.getDate()+1);
     var tomorrowFormat=new DatePipe('en-US').transform(tomorrow, "MMM d, y")
     
-    if(remiderDate==tomorrowFormat)
+    if(reminderDate==tomorrowFormat)
     {
       return "tomorrow"+" "+time;
     }
-    return remiderDate+","+time;
+
+    var yesterday=today.setDate(today.getDate()-2);
+    var yesterdayFormat=new DatePipe('en-US').transform(yesterday, "MMM d, y");
+    
+    if(yesterdayFormat==reminderDate){
+      return "yesterday"+" "+time;
+    }
+    return reminderDate+","+time;
   }
 
 }
